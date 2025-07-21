47d523645e5f2f39af627725a50f5a6685c075e1
Merve Özen <mervehediyeozen98@gmail.com>
2025-07-22 01:24:51 +0300

    everything added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Main from './pages/main';
+import Signin from './pages/signin';
+import { Route , Routes } from 'react-router-dom';
+import User from './pages/user';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import NewAccount from './pages/newAccount';
+import { createAccount } from './redux/testSlice';
+import Champions from './pages/champions';
+import Article1 from './pages/articles/article1';
+import Article2 from './pages/articles/article2';
 function App() {
+    const dispatch = useDispatch();
+  useEffect(() => {
+    const storedUser = localStorage.getItem("userInfo");
+    if (storedUser) {
+      const parsedUser = JSON.parse(storedUser);
+      dispatch(createAccount(parsedUser)); // Otomatik login
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {/*Pages */} 
+     <Routes> 
+       <Route path="/" element={<Main />} />
+       <Route path="/signin" element={<Signin />} />
+       <Route path="/user" element={<User />} />
+       <Route path="/createnew" element={<NewAccount />} />
+       <Route path="/champions" element={<Champions />} />
+       <Route path="/article1" element={<Article1 />} />
+       <Route path="/article2" element={<Article2 />} />
+     </Routes>
+    </>
   );
 }
 
